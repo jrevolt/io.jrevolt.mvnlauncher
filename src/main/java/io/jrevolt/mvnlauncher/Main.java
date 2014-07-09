27925803d6d0a91f99d1019c9562a44984bb1244
Patrik Beno <patrik.beno@greenhorn.sk>
2014-07-09 23:59:11 +0200

    delegate to SpringBoot MvnLauncher

--- src/main/java/io/jrevolt/mvnlauncher/Main.java
+++ src/main/java/io/jrevolt/mvnlauncher/Main.java
@@ -12,14 +12,7 @@ import java.util.regex.Pattern;
 public class Main {
 
 	static public void main(String[] args) {
-		String artifact = (args.length > 0) ? args[0] : null;
-		if (artifact == null || !artifact.matches("[^:]+(:[^:]+){2,5}")) {
-			throw new RuntimeException("Undefined or invalid main artifact. First parameter must specify Maven artifact URI ({groupId}:{artifactId}:{version})");
-		}
-		System.setProperty("MvnLauncher.artifact", artifact);
-		String[] newargs = new String[args.length - 1];
-		System.arraycopy(args, 1, newargs, 0, newargs.length);
-		MvnLauncher.main(newargs);
+		MvnLauncher.main(Main.class, args);
 	}
 
 }
